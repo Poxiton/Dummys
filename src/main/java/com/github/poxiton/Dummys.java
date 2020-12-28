6ca004b9ca20f383c4b8420fe8267c61325893f5
Ksawery Skoczylas <poxitonploner@gmail.com>
2020-12-28 21:04:53 +0100

    Added Update Checker

--- src/main/java/com/github/poxiton/Dummys.java
+++ src/main/java/com/github/poxiton/Dummys.java
@@ -1,9 +1,12 @@
 package com.github.poxiton;
 
+import java.util.logging.Logger;
+
 import com.github.poxiton.commands.DummyCommand;
 import com.github.poxiton.events.DamageListener;
 import com.github.poxiton.utils.CommandCompletion;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,6 +18,16 @@ public class Dummys extends JavaPlugin {
     public void onEnable() {
         this.saveDefaultConfig();
         FileConfiguration config = this.getConfig();
+
+        Logger logger = this.getLogger();
+
+        new UpdateChecker(this, 87188).getVersion(version -> {
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                logger.info(ChatColor.GREEN + "There is no updates available.");
+            } else {
+                logger.info(ChatColor.RED + "There is an update available.");
+            }
+        });
 
         manager = new DummyManager(this);
 
